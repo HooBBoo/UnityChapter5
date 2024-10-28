fd35705b177735a7a86b748c47f3e09a3623335d
HooBBoo <wldbs272@naver.com>
2024-10-28 10:28:23 +0900

    [Update] Item Interaction

---
+++ Assets/01.Scripts/Player/Interaction.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f;  //ray를 얼마나 자주 쏠 지
+    private float lastCheckTime;     //ray를 마지막으로 쏜 시간
+    public float maxCheckDistance;   //얼마나 멀리있는 것을 체크할지
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject; //인터렉션 된 게임 오브젝트의 정보
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));  //정중앙에서 쏘려고 스크린 너비,높이의 반
+            RaycastHit hit;  //부딪힌 오브젝트
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    Debug.Log("레이 맞음!");
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else //빈 공간에 레이 쏠 때
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetInteractPrompt();
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
